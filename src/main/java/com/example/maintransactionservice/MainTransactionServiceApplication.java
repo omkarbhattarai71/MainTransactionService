5cd7e19088685313f12faf4d24e65a3586fb5b0d
Omkar Bhattarai <omkarbhattarai71@gmail.com>
2023-06-27 03:45:44 +0530

    First Comit
    Beginning of Microservices

---
+++ src/main/java/com/example/maintransactionservice/MainTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainTransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainTransactionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maintransactionservice/MainTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTransactionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
